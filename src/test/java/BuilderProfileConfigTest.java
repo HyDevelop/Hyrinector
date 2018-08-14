c09ce8de25aacb940d20840ee3fe43b10c5b315d
Hykilpikonna <admin@moecraft.cc>
2018-08-14 09:18:45 +0800

    [T] Test builder profile config

---
+++ src/test/java/BuilderProfileConfigTest.java
@@ -0,0 +1,19 @@
+import cc.moecraft.osu.builder.BuilderProfileConfig;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/13 创建!
+ * Created by Hykilpikonna on 2018/08/13!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class BuilderProfileConfigTest
+{
+    public static void main(String[] args)
+    {
+        BuilderProfileConfig config = new BuilderProfileConfig("./src/main/resources");
+
+        System.out.println(config);
+    }
+}
